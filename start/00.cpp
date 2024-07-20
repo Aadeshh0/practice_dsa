f53d820d40a8467e860aecbb6b9ed502e7a8c29c
Aadeshh0 <ag9494@srmist.edu.in>
2024-07-21 02:49:15 +0530

    setup i/o

---
+++ start/00.cpp
@@ -0,0 +1,11 @@
+#include "bits/stdc++.h"
+// using namespace std;
+
+int main(){
+    
+    int x, y;
+    std::cin >> x >> y;
+    std::cout << "value of x: " << x << "value of y" << y;
+    return 0;
+
+}
---
+++ start/01.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+
+
+
+int main(){
+    int num1 = 15, num2 = 12;
+
+    int sum = num1 + num2;
+    std::cout <<"the value of sum is: "<< sum << std::endl;
+    return 0;
+}
